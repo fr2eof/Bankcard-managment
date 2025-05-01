41190b4ba335548fb5ed92b39f30bb41eb5d5fef
Ivanov Ilya <iiilya12@yandex.ru>
2025-05-01 20:24:13 +0300

    main test class

---
+++ src/test/java/com/test/card_system/CardSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.card_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
