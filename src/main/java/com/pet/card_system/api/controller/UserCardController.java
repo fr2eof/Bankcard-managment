ef2b4cf0a26cba0945ed0bb262d228b6728337b8
Ivanov Ilya <iiilya12@yandex.ru>
2025-05-05 21:07:41 +0300

    feat: finished UserCardController

--- src/main/java/com/pet/card_system/api/controller/UserCardController.java
+++ src/main/java/com/pet/card_system/api/controller/UserCardController.java
@@ -1,39 +1,60 @@
 package com.pet.card_system.api.controller;
 
-import com.pet.card_system.core.dto.CardDTO;
-import com.pet.card_system.core.dto.TransferRequest;
-import com.pet.card_system.core.dto.TransferResponse;
+import com.pet.card_system.core.dto.*;
+import com.pet.card_system.core.repository.entity.CardStatus;
+import com.pet.card_system.security.service.CardService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/cards")
 @RequiredArgsConstructor
 public class UserCardController {
+    private final CardService cardService;
 
-    @GetMapping
-    public ResponseEntity<List<CardDTO>> getUserCards(
+    @GetMapping("/{userId}")
+    public Page<CardDTO> getUserCards(
+            @PathVariable Long userId,
+            @RequestParam(required = false) String cardType,
             @RequestParam(required = false) String status,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size) {
-        return null;
-        // Логика получения карт пользователя
+            @RequestParam(required = false) BigDecimal minBalance,
+            @RequestParam(required = false) BigDecimal maxBalance,
+            @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+
+        CardSearchCriteria criteria = CardSearchCriteria.builder()
+                .cardType(cardType)
+                .status(CardStatus.valueOf(status))
+                .minBalance(minBalance)
+                .maxBalance(maxBalance)
+                .build();
+
+        return cardService.getUserCards(userId, criteria, pageable);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<CardDTO> getCardDetails(@PathVariable Long id) {
-        return null;
-        // Получение деталей конкретной карты
+    @GetMapping("/{cardId}")
+    public ResponseEntity<CardDTO> getCardDetails(@PathVariable Long cardId) {
+        CardDTO card = cardService.getCardDetails(cardId);
+        return ResponseEntity.ok(card);
     }
 
-    @PostMapping("/{id}/block")
-    public ResponseEntity<Void> requestBlockCard(@PathVariable Long id) {
-        return ResponseEntity.ok().build();
-        // Запрос на блокировку карты
+    @PostMapping("/{cardId}/action")
+    public ResponseEntity<Void> requestBlockCard(
+            @PathVariable Long cardId,
+            @RequestBody @Valid CardActionRequestCreateDTO requestCreateDTO
+    ) {
+        cardService.createRequest(cardId, requestCreateDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
 
     @PostMapping("/transfer")
     public ResponseEntity<TransferResponse> transferBetweenCards(
@@ -42,21 +63,3 @@ public class UserCardController {
         // Перевод между картами пользователя
     }
 }
-/*
-*
-*
-*
-* Просматривать свои карты (с параметризованным поиском и постраничной выдачей).
-Запрашивать блокировку карты.
-Совершать переводы между своими картами, если у него есть несколько карт.
-Просматривать баланс по своим картам.
-*
-*
-* ✅ API:
-Эндпоинты для CRUD-операций над картами.
-Операции перевода средств между картами пользователя.
-Параметризированный поиск и постраничная выдача списка карт.
-Валидация входящих данных.
-*
-*
-* */
