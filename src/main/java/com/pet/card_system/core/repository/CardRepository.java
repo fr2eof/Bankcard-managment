04d87cfd9e5851ae030ca407ee532a818f434371
Ivanov Ilya <iiilya12@yandex.ru>
2025-05-05 21:05:15 +0300

    feat: divide getting cards on with predicates or not

--- src/main/java/com/pet/card_system/core/repository/CardRepository.java
+++ src/main/java/com/pet/card_system/core/repository/CardRepository.java
@@ -1,14 +1,20 @@
 package com.pet.card_system.core.repository;
 
 import com.pet.card_system.core.repository.entity.Card;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
-import java.util.List;
-import java.util.Optional;
 
-public interface CardRepository extends JpaRepository<Card, Long> {
-    List<Card> findByUserId(Long userId);
+public interface CardRepository extends JpaRepository<Card, Long>, JpaSpecificationExecutor<Card> {
+    Page<Card> findByUserId(Long userId, Pageable pageable);
 
-    Optional<Card> findByIdAndUserId(Long cardId, Long userId);
-
+    @NonNull
+    @EntityGraph(attributePaths = {"user"})
+    Page<Card> findAll(@Nullable Specification<Card> spec, @NonNull Pageable pageable);
 }
